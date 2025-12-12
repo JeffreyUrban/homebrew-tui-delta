f259c9f8f163fb7036cbb3520cf2bae7300cd9e3
JeffreyUrban <2358257+JeffreyUrban@users.noreply.github.com>
2025-12-12 00:56:09 +0000

    chore: Update tui-delta to v0.1.1

--- Formula/tui-delta.rb
+++ Formula/tui-delta.rb
@@ -3,8 +3,8 @@ class TuiDelta < Formula
 
   desc "Wraps a TUI (terminal user interface) applicatvel content"
   homepage "https://github.com/JeffreyUrban/tui-delta"
-  url "https://files.pythonhosted.org/packages/TEMPLATE_PLACEHOLDER/tui_delta-0.1.0.tar.gz"
-  sha256 "TEMPLATE_PLACEHOLDER_SHA256"
+  url "https://files.pythonhosted.org/packages/9b/c4/9b7caa9d282799c9c4a942553adc269bd2fa005b658b5021d5524534aef6/tui_delta-0.1.1.tar.gz"
+  sha256 "689fbb97cf29fe769d647cf8cb93b6880e328285ff3bdf780ebea3ec493beffb"
   license "MIT"
 
   depends_on "python@3.9"
